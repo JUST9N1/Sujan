bc89d78552677c8fe1eda901af46193378999c2b
Sujan Gautam <susangautam062@gmail.com>
2022-05-14 17:39:50 +0545

    Number Guessing Game

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-import pygame
+import random
+
+def guess(x):
+    count = 0
+    random_number = random.randint(1, x)
+    guess = 0
+    while random_number != guess:
+        count += 1
+        guess = int(input("Enter a number between 1 and {}".format(x)))
+        if guess < random_number:
+            print("Number is too low")
+        elif guess > random_number:
+            print("Number is too high")
+        else:
+
+            print(f"You've Guessed the number in {count} times. \nCongratulation!")
+
+choice = int(input("Enter a number: "))
+guess(choice)
